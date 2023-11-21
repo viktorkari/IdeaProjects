c21ec924b65aeae8f7f79def1db0bc7d6d738feb
Vikto <viktorkarlin36@gmail.com>
2023-11-21 14:29:29 +0200

    js

---
+++ js/task1.js
@@ -0,0 +1,52 @@
+/*-------------word-----------------*/
+let s1 = "hello";
+console.log(s1);
+let s2 = "own";
+console.log(s2);
+let s3 = "com";
+console.log(s3);
+let s4 = "ua";
+console.log(s4);
+/*------------------number-----------*/
+let n1 = 1;
+console.log(n1);
+let n2 = 10;
+console.log(n2);
+let n3 = -999;
+console.log(n3);
+let n4 = 123;
+console.log(n4);
+let n5 = 3.14;
+console.log(n5);
+let n6 = 2.7;
+console.log(n6);
+let n7 = 16;
+console.log(n7);
+/*-----------------boolean----------*/
+let f = 3>5;
+console.log(f);
+let t = 5!==4;
+console.log(t);
+/*-----------Name---------------*/
+let firstName ="Viktor ";
+let middleName = "Genadievich ";
+let lastName = "Karlin";
+person = String(firstName + middleName + lastName);
+console.log(person);
+/*-------------typeof------------*/
+let a = 100;
+let b = "100";
+let c = true;
+console.log(typeof a, b, c );
+/*-------------prompt`on----------*/
+let age=prompt('Скількі вам років?',39);
+alert(`Вам ${age} лет!`);
+let firstname=prompt('Ваше імя?' ,"Віктор");
+alert(`Ваше ${firstname} імя!`);
+let middlename=prompt('Ваше очество?',"Геннадійович");
+alert(`Ваше ${middlename} очество!`);
+console.log(age, firstname, middlename);
+
+
+
+
